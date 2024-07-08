b57d369bcd556cbe122e96ae4130c33532f9ca12
Talib <abutalib715@gmail.com>
2024-07-08 14:50:21 +0600

    setup as a maven project

--- src/java/com/tech/blog/helper/ConnectionProvider.java
+++ src/java/com/tech/blog/helper/ConnectionProvider.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tech.blog.helper;
 import java.sql.*;
-
 
 public class ConnectionProvider {
     private static Connection con;
